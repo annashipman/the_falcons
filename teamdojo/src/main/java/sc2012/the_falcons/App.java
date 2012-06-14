4491e53f4173322a3b8fa95f554410180ddfb3c5
gemcfadyen <gemcfadyen@hotmail.co.uk>
2012-06-14 14:46:11 +0100

    i Please enter the commit message for your changes. Lines starting

---
+++ teamdojo/src/main/java/sc2012/the_falcons/App.java
@@ -0,0 +1,13 @@
+package sc2012.the_falcons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
